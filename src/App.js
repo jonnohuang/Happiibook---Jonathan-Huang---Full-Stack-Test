50d0d2a098a456ee8872fc76919b4338eb7496f2
Jonathan Huang <jonnohuang@hotmail.com>
2020-12-07 15:10:34 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import DayPicker from "./Components/DayPicker";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Happiibook</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <DayPicker />
+    </div>
+  );
+}
